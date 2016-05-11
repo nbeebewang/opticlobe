2ee6cd7f5e6adb0471f0071dd11649a5dc51af25
nbeebewang <nbeebewang@college.harvard.edu>
2016-05-11 16:35:57 -0400

    stuff

--- bam_to_sam.sh
+++ bam_to_sam.sh
@@ -6,7 +6,7 @@
 #SBATCH -p serial_requeue          # partition (general,serial_requeue)
 #SBATCH -n 1         # number of cores
 #SBATCH -N 1         # number of nodes
-#SBATCH --mem 4000           # memory pool for all cores
+#SBATCH --mem 6000           # memory pool for all cores
 #SBATCH -t 0-01:00         # time (D-HH:MM)
 #SBATCH -o ../log/bam_to_sam_%A.out          # STDOUT
 #SBATCH -e ../log/bam_to_sam_%A.err         # STDERR
